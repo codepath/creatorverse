0d77d7431c2bfaa94221e234ae1687a92aa2816c
jamjamgobambam <jamila.khawaja85@gmail.com>
2025-12-17 21:12:08 -0600

    update supabase db

--- client/src/client.js
+++ client/src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const URL = 'https://kvfmhdalziozcdcpheor.supabase.co';
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2Zm1oZGFsemlvemNkY3BoZW9yIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYxNzIyMDksImV4cCI6MTk5MTc0ODIwOX0.yyHmQbUaOhuUXsJCU6DgaBNhTIiOmd7DcSIqHecBXWc';
+const URL = 'https://dhkzlhezspqtupcdgtlx.supabase.co';
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRoa3psaGV6c3BxdHVwY2RndGx4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwMjI0MTgsImV4cCI6MjA4MTU5ODQxOH0.mznedbRFi1O6pVcvEfYUjsaZN66YD3K4Z336QSWbSWU';
 
 export const supabase = createClient(URL, API_KEY);
